b0d12a08a2a04b2f2f4bb17bdccdbd1a0faa18f2
Willian Kenneth de Souza <willianksouza94@gmail.com>
2025-03-07 14:35:59 -0300

    update 04

--- resources/views/components/theme/footer.blade.php
+++ resources/views/components/theme/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="bg-zinc-900 h-[530px] -z-20">
+<footer class="bg-zinc-900 -z-20">
     <x-container>
-        <div class="grid grid-cols-2 text-zinc-300">
+        <div class="grid sm:grid-cols-2 gap-8 text-zinc-300">
             <div class="flex flex-col gap-4">
                 <h1 class="font-black text-2xl">INFORMATION</h1>
                 <p class="text-lg text-zinc-400">Lorem ipsum dolor sit amet, consectetur<br> adipiscing elit. Ut elit tellus, luctus nec<br> ullamcorper mattis, pulvinar dapibus leo.</p>
@@ -19,7 +19,7 @@
                     </li>
                   </ul>
             </div>
-            <div class="grid grid-cols-2 ">
+            <div class="grid md:grid-cols-2 gap-4">
                 <nav>
                     <h1 class="font-black text-2xl">NAVIGATION</h1>
                     <ul class="space-y-4 mt-2">
@@ -73,11 +73,26 @@
                             <span class="text-lg">Hello@Homco.com</span>
                         </li>
                     </ul>
-                        <form class="space-y-4">
-                            <input class="bg-zinc-600 py-6 px-6" type="text" placeholder="Email Address" class="bg-zinc-800 text-zinc-300 rounded-md">
-                            <button class="bg-zinc-500 text-zinc-300 py-6 px-16">SUBSCRIBE</button>
+                        <form class="flex flex-col items-start gap-4">
+                            <input class="max-w-full bg-zinc-600 py-6 px-4" type="text" placeholder="Email Address" class="bg-zinc-800 text-zinc-300">
+                            <button class="bg-zinc-500 text-zinc-300 py-4 px-8 ">SUBSCRIBE</button>
                         </form>
                 </div>
+            </div>
+            <div class="md:col-span-2 border-t">
+                <x-container>
+                    <div class="grid md:grid-cols-2 justify-items-center md:justify-items-normal py-8">
+                        <h1 class="font-semibold text-xs md:text-lg uppercase tracking-wider">Allright Reserved - Homco Interior</h1>
+                        <ul class="flex gap-8 md:gap-12 justify-self-auto md:justify-self-end">
+                            <li class="font-semibold text-xs md:text-lg uppercase tracking-wider">DIsclaimer</li>
+                            <li class="font-semibold text-xs md:text-lg uppercase tracking-wider text-nowrap">Privacy Policy
+                            </li>
+                            <li class="font-semibold text-xs md:text-lg uppercase tracking-wider text-nowrap">
+                                Term Of Use
+                            </li>
+                        </ul>
+                    </div>
+                </x-container>
             </div>
         </div>
     </x-container>
